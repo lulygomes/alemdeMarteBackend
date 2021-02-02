6c4a1d33f0fcf1a09874e7b827381b3a48baa31d
Luiz Silva <luiz.gsilva42@gmail.com>
2021-02-02 08:47:09 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
-
+import cors from 'cors';
 import routes from './routes';
 
 import createConnection from './database';
@@ -9,6 +9,7 @@ createConnection();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
